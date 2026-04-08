0b057dedb64fde7dec6c52c4bda6d31bda8c3053
TxENGAx <137093780+TxENGAx@users.noreply.github.com>
2026-04-08 11:16:19 +0300

    Update passwallx.sh

--- passwallx.sh
+++ passwallx.sh
@@ -84,7 +84,7 @@ echo "Installing Passwall 1 ..."
 
 sleep 2
 
-rm -f passwall.sh && wget https://raw.githubusercontent.com/amirhosseinchoghaei/Passwall/main/passwall.sh && chmod 777 passwall.sh && sh passwall.sh
+rm -f passwall.sh && wget https://raw.githubusercontent.com/TxENGAx/Passwall2-Openwrt-25/main/passwall.sh && chmod 777 passwall.sh && sh passwall.sh
 
 
 ;;
@@ -95,7 +95,7 @@ echo "Installing Passwall 2 ..."
 
 sleep 2
 
-rm -f passwall2x.sh && wget https://raw.githubusercontent.com/amirhosseinchoghaei/Passwall/main/passwall2x.sh && chmod 777 passwall2x.sh && sh passwall2x.sh
+rm -f passwall2x.sh && wget https://raw.githubusercontent.com/TxENGAx/Passwall2-Openwrt-25/main/passwall2x.sh && chmod 777 passwall2x.sh && sh passwall2x.sh
 
  
 ;;
